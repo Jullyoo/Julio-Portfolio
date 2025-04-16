b8a0606ae5ad241b1d0d111f15ff38a8f5d2b919
Jullyoo <julio.guimaraes2020@outlook.com>
2025-04-16 18:02:48 -0300

    First Commit

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,6 +1,6 @@
 document.addEventListener("scroll", function () {
     const header = document.getElementById("header");
-    const secondSection = document.querySelector(".about_section_center"); 
+    const secondSection = document.querySelector(".about_section"); 
     const sectionPosition = secondSection.getBoundingClientRect().top; 
     if (sectionPosition <= 0) {
         header.classList.add("scrolled"); 
